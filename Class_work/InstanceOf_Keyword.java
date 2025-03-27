53cdf0d5a036d85b5718dfabdba4f61163bf0a36
Varun K V <varunvishwanath1@gmail.com>
2025-03-27 15:25:00 +0530

    Create InstanceOf_Keyword.java

---
+++ Class_work/InstanceOf_Keyword.java
@@ -0,0 +1,16 @@
+package Java;
+
+class Inst{
+	
+}
+
+public class InstanceOf_Keyword {
+
+	public static void main(String[] args) {
+		Inst obj=new Inst();
+		System.out.println(obj instanceof Inst);
+		
+
+	}
+
+}
