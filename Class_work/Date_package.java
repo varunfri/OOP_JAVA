e9b943f83f2d3f748822eb2e6910b4f090f313e3
Varun K V <varunvishwanath1@gmail.com>
2025-03-31 21:34:09 +0530

    Create Date_package.java

---
+++ Class_work/Date_package.java
@@ -0,0 +1,13 @@
+import java.util.Date;  // use this api to get data from device
+
+public class Date_package {
+
+    public static void main(String[] args) {
+        System.out.println("Current Date");
+        System.out.println("------------");
+
+        // Create a Date object
+        Date currentDate = new Date();
+        System.out.println(currentDate);
+    }
+}
